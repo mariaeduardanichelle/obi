05cceeed11a5b10e84c02e88417e013847fbe520
Maria Eduarda Nichelle <95757504+mariaeduardanichelle@users.noreply.github.com>
2023-03-12 21:51:38 -0300

    Update obi.py

--- obi.py
+++ obi.py
@@ -1,9 +1,7 @@
 n1 = int(input('Digite a idade: '))
 n2 = int(input('Digite a idade: '))
 n3 = int(input('Digite a idade: '))
-if n1 >= n2 and n1 <= n3 or n1 <= n2 and n1 >= n3:
-    print(n1)
-elif n2 >= n1 and n2 <= n3 or n2 <= n1 and n2 >= n3:
-    print(n2)
-elif n3 >= n1 and n3 <= n2 or n3 <= n1 and n3 >= n2:
-    print(n3)
+id = [n1, n2, n3]
+id.remove(max(id))
+id.remove(min(id))
+print(id[0])
