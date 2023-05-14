e9e3158b277dadc29e2d22560cb2e946f98183bd
Maria Eduarda Nichelle <madunichelle@gmail.com>
2023-05-14 20:37:18 -0300

    Update cartas2014.py

--- cartas2014.py
+++ cartas2014.py
@@ -3,14 +3,10 @@ ordem = []
 ordemd = []
 for i in range(0, 5):
     cartas.append(int(input('Digite o número da carta: ')))
-for v in range(0, 5):
-    ordem.append(cartas[v])
-    ordemd.append(cartas[v])
+ordem = cartas[:]
+ordemd = cartas[:]
 ordem.sort()
 ordemd.sort(reverse=True)
-print(cartas)
-print(ordem)
-print(ordemd)
 if cartas == ordem:
     print('C')
 elif cartas == ordemd:
